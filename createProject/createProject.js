04661055e3442b6fbbcf1a71a30aa359b9848291
Chris Shafer <cshafer1@nd.edu>
2020-09-16 14:45:49 -0500

    Update code

---
+++ createProject/createProject.js
@@ -0,0 +1,9 @@
+/*--------------------- Home Component ---------------------*/
+const createProject = {
+    templateUrl: '/createProject/createProjectForm.html'
+}
+
+// Home Component with Routing (Routed / Stateful)
+angular.module('app').component('create', createProject);
+/*--------------------- Home Component ---------------------*/
+
---
+++ utility/utility.js
@@ -0,0 +1,9 @@
+/*--------------------- Home Component ---------------------*/
+const navbar = {
+    templateUrl: '/utility/navbar.html'
+}
+
+// Home Component with Routing (Routed / Stateful)
+angular.module('app').component('navbar', navbar);
+/*--------------------- Home Component ---------------------*/
+
