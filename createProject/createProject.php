670570a139359b593cd7a6d50745abc4c006dab3
Chris Shafer <cshafer1@nd.edu>
2020-09-16 15:44:08 -0500

    Add working input form

--- createProject/createProject.php
+++ createProject/createProject.php
@@ -1,4 +1,23 @@
 <?php
-var_dump($_POST);
-
+    if (sizeof($_POST) > 0){
+        $new_data = array(
+                'projectName' => $_POST['projectName'],
+                'projectMaxMembers' => $_POST['projectMaxMembers'],
+                'projectCategory' => $_POST['projectCategory'],
+                'projectImage' => $_POST['projectImage'],
+                'projectDesc' => $_POST['projectDesc'],
+                'projectPrivate' => $_POST['projectPrivate']
+            );
+        if(file_exists('project_data.json')){
+            $current = file_get_contents('project_data.json');
+            $current_arr = json_decode($current, true);
+            $current_arr[] = $new_data;
+            $final_data = json_encode($current_arr);
+            file_put_contents('project_data.json', $final_data);
+        } else {
+            $data = json_encode($new_data);
+            file_put_contents('project_data.json', $final_data);
+        }
+    }
+    header("Location: /index.html", true, 301);
 ?>
