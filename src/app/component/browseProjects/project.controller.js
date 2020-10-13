04a7980652d9913878bfcbb8c17887769d96032b
cshafer1 <nknscripts@gmail.com>
2020-10-13 18:04:06 -0400

    Fix projects grid

--- src/app/component/browseProjects/project.controller.js
+++ src/app/component/browseProjects/project.controller.js
@@ -2,9 +2,13 @@
 
 function ProjectController(){
     const $ctrl = this;
-    $ctrl.imageview = true;
-    if($ctrl.project["image"] == '' || $ctrl.project["image"] == 'optional')
-        $ctrl.imageview = false;
+    
+    $ctrl.$onInit = function(){
+        console.log("control is: ", $ctrl);
+        $ctrl.imageview = true;
+        if($ctrl.project["image"] == '' || $ctrl.project["image"] == 'optional')
+            $ctrl.imageview = false;
+    }
 }
 
 angular.module('components.projectGrid').controller('ProjectController', ProjectController);
